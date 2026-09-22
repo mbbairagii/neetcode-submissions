e80ae8b210f2592064881d477813ec4a3392abe5
Mohini <mbbairagii@gmail.com>
2026-09-23 00:06:26 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>>mp;
+        for(string& s: strs){
+            string key=s;
+            sort(key.begin(),key.end());
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        for(auto& [key, group]:mp){
+            ans.push_back(group);
+        }
+
+        return ans;
+    }
+};
